a579ff614b93ebc98b23613009b4e01f082af9e2
Petiso13 <ian13celestino@gmail.com>
2024-08-28 21:45:46 -0300

    CLI done. TODO: deletion option

--- script.py
+++ script.py
@@ -1,50 +1,56 @@
 import os
 import asyncio
+from dataclasses import dataclass
 from dotenv import load_dotenv
 from datetime import datetime, timezone, timedelta
 from telethon import TelegramClient
 from telethon import utils
 from telethon import functions
 
-# Getting the api id and api hash from the .env file
-# The .env file should be in the same directory as the script
-# Get the api id and api hash from https://my.telegram.org
-load_dotenv()
-api_id = os.getenv('API_ID')
-api_hash = os.getenv('API_HASH')
-
-
-# The name of the group chat you want to get the members from and the date limit
-target_group_name = ''
-day_limit = 21
-month_limit = 8
-year_limit = 2024 
-
+@dataclass
+class UserInfo:
+    participated: bool 
+    username: str
+    name: str
 
 async def main():
 
+    if load_dotenv():
+        api_id = os.getenv('API_ID')
+        api_hash = os.getenv('API_HASH')
+    else:
+        print('You have no .env file, one will be created to store your Telegram app data for future use.')
+        with open('.env', 'w') as file:
+            api_id = input('Please enter your API id\n')
+            api_hash = input('Please enter your API hash\n')
+            file.writelines(f'API_ID = {api_id}\n', f'API_HASH = {api_hash}\n')
     client = TelegramClient('anon', api_id, api_hash)
+    target_group_name = input('Please enter the group name you would like me to scan.\n--> ')
+    print('Up until when would you like me to scan?', end=' ')
+    year_limit = int(input('Please enter the year.\n--> ')) 
+    month_limit = int(input('Please enter the month.\n--> ')) 
+    day_limit = int(input('Please enter the day.\n--> ')) 
     datetime_limit = datetime(year_limit, month_limit, day_limit, tzinfo=timezone.utc)
 
     await client.start()
     chats = await client.get_dialogs()
-    group_chat = list(filter(lambda chat: chat.name == target_group_name, chats))[0]
+    group_chat = list(filter(lambda chat: chat.name.startswith(target_group_name), chats))[0]
     all_members = await client.get_participants(group_chat)
-    members_dict_id_to_username_name = dict()
+    members_dict_id_to_userinfo = dict()
     for member in all_members:
-        members_dict_id_to_username_name[member.id] = [member.username, member.first_name]
+        members_dict_id_to_userinfo[member.id] = UserInfo(False, member.username, member.first_name)
 
     async for message in client.iter_messages(group_chat):
         if datetime_limit <= message.date:
-            if message.sender_id in members_dict_id_to_username_name:
-                del members_dict_id_to_username_name[message.sender_id]
+            members_dict_id_to_userinfo[message.sender_id].participated = True
             if message.reactions:
                 for reaction in message.reactions.recent_reactions:
-                    if reaction.peer_id in members_dict_id_to_username_name:
-                        del members_dict_id_to_username_name[reaction.peer_id]
-        break
-    for member_id in members_dict_id_to_username_name: 
-        print(members_dict_id_to_username_name[member_id]) 
+                    members_dict_id_to_userinfo[reaction.peer_id.user_id].participated = True
+        else:
+            break
+    for member_id in members_dict_id_to_userinfo: 
+        if not members_dict_id_to_userinfo[member_id].participated:
+            print(f'{members_dict_id_to_userinfo[member_id].username}, {members_dict_id_to_userinfo[member_id].name}')
 
 
 asyncio.run(main())
